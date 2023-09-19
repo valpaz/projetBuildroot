721093f113c38b36fdffbc64166e0e84a2f10c7b
valpaz <aguilar.jeandavid@yahoo.com>
2023-09-19 09:25:28 +0200

    ajout

---
+++ README.md
@@ -0,0 +1,1 @@
+g++ -std=c++17 -o ahah main.cpp -lcurl -lcryptopp
--- main.cpp
+++ main.cpp
@@ -11,20 +11,13 @@
 #include <cryptopp/filters.h>
 #include <cryptopp/files.h>
 #include <filesystem>
+#include <iomanip>
+#include <ctime>
+#include <sstream>
 
 // A installer sur buildroot : curl (libcurl4-openssl-dev), Crypto++ (libcrypto++-dev)
 using namespace std;
 
-// Fonction de rappel pour traiter les données reçues ligne par ligne
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::vector<std::string>* lines) {
-    size_t total_size = size * nmemb;
-    string line(static_cast<char*>(contents), total_size);
-
-    // Stocker la ligne dans le conteneur (par exemple, un vecteur)
-    lines->push_back(line);
-
-    return total_size;
-}
 string calculateMD5(const std::string& filename) {
     CryptoPP::Weak1::MD5 md5;
     string result;
@@ -35,6 +28,24 @@ string calculateMD5(const std::string& filename) {
     return md5Hex;
 }
 
+std::string getDateMinus7Days(const std::string& inputDate) {
+    // Convertissez la date d'entrée en une structure tm
+    std::tm timeinfo = {};
+    std::istringstream ss(inputDate);
+    ss >> std::get_time(&timeinfo, "%Y%m%d");
+
+    // Soustrayez 7 jours en secondes (7 jours * 24 heures * 60 minutes * 60 secondes)
+    std::time_t timestamp = std::mktime(&timeinfo) - (1 * 24 * 60 * 60);
+
+    // Convertissez le résultat en une nouvelle structure tm
+    std::tm resultTimeinfo = *std::localtime(&timestamp);
+
+    // Formatez la nouvelle date en tant que chaîne de caractères au format "YYYYMMDD"
+    std::ostringstream oss;
+    oss << std::put_time(&resultTimeinfo, "%Y%m%d");
+
+    return oss.str();
+}
 int main() {
 
     cout << "LAAAAA" << endl;
@@ -62,12 +73,12 @@ int main() {
         if (resInit != CURLE_OK) {
             cerr << "Error :" << curl_easy_strerror(resInit) << endl;
         }else {
-            cout << "Requette d'authentification réussie " << endl;
+            cout << "Requette réussie : Mise en place de l'authentification" << endl;
             string filename = "license_terms.txt";
             md5 = calculateMD5(filename);
         }
     }
-    if (!std::filesystem::exists("output.txt")){
+    if (!std::filesystem::exists("individu.txt")){
         // GET INDIVIDUAL FILE //
         string adresse = "https://www.movebank.org/movebank/service/direct-read?entity_type=individual&study_id=1266784970&license-md5="+md5; // Remplacez par l'URL de votre choix
         const char* url = adresse.c_str();
@@ -86,35 +97,107 @@ int main() {
         if (res != CURLE_OK) {
             cerr << "Erreur lors de la requête cURL : \n" << endl;
         }else {
-            cout << "Requette réussie : \n" << endl;
+            cout << "Requette réussie : Mise en place Récupération des individues\n" << endl;
         }
         fclose(outputFile);
-        curl_easy_cleanup(curl);
-    }
+        //curl_easy_cleanup(curl);
+        // PARSE INDIVIDUAL FILE AND CREATE OBJECT //
+        string line;
+        ifstream fileOutput("output.txt");
+        ofstream fileIndiv("individu.txt", std::ios::out | std::ios::trunc);
+        while (getline(fileOutput, line)) {
+            stringstream ss(line);
+            int cpt =0;
+            while (ss.good()) {
+                std::string substr;
+                getline(ss, substr, ',');
+                if (cpt == 7){
+                    if (isdigit(substr[0])){
+                        fileIndiv << substr << std::endl;
+                    }
+                    cout << substr << endl;
+                }
+                cpt++;
+            }
 
-    // PARSE INDIVIDUAL FILE AND CREATE OBJECT //
-    string line;
-    ifstream fileIndiv("output.txt");
-    while (getline(fileIndiv, line)) {
-        stringstream ss(line);
-        int cpt =0;
-        while (ss.good()) {
-            std::string substr;
-            getline(ss, substr, ',');
-            if (cpt == 6){
-                cout << substr << endl;
-            }
-            if (cpt == 8){
-                cout << substr << endl;
-            }
-            if (cpt == 23){
-                cout << substr << endl;
-            }
-            cpt++;
         }
+        fileIndiv.close();
+        fileOutput.close();
+        remove ("output.txt");
+
+
+        // Recuperation initiale des events //
+        auto t = time(nullptr);
+        auto tm = *localtime(&t);
+        ostringstream oss;
+        oss << put_time(&tm, "%Y%m%d%H%M%S000");
+        auto actualDate = oss.str();
+        string past7date=getDateMinus7Days(actualDate)+actualDate.substr(8, 9);;
+        cout << "ICI" << endl;
+        cout << past7date << endl;
+        cout << actualDate << endl;
+
+        string adresse2 = "https://www.movebank.org/movebank/service/direct-read?entity_type=event&study_id=1266784970&timestamp_start="+past7date+"&timestamp_end="+actualDate+"&license-md5="+md5;
+        const char* url2 = adresse2.c_str();
+        // Configuration de l'URL
+        CURLcode res2;
+        curl_easy_setopt(curl, CURLOPT_URL, url2);
+        curl_easy_setopt(curl, CURLOPT_USERPWD, "valpaz:longboard09");
+        curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "cookies.txt");
+        FILE* outputEvent;
+        outputEvent = fopen("outputEvent.csv", "wb");
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, outputEvent);
+        // Exécution de la requête
+        res2 = curl_easy_perform(curl);
+        // Vérification de la réponse
+        if (res2 != CURLE_OK) {
+            cerr << "Erreur lors de la requête cURL : \n" << endl;
+        }else {
+            cout << "Requette réussie : Récuperation initial des 7 derniers jours d'event pour chaque individue\n" << endl;
+        }
+        curl_easy_cleanup(curl);
+
+        ifstream fileEvent("outputEvent.csv");
+        ifstream fileInds("individu.txt");
+        string ligne;
+        string ligneEvent;
+        string actualId;
+        int cptt;
+        while (std::getline(fileInds, ligne)) {
+            actualId = ligne;
+            ofstream fileInd(("fichierInd/"+actualId + ".csv").c_str());
+            while (getline(fileEvent, ligneEvent)) {
+                stringstream ss(ligneEvent);
+                string valeur;
+                cptt = 0;
+                while (getline(ss, valeur, ',')) {
+                    if (cptt==4){
+                        cout << "\n" << endl;
+                        cout << valeur << endl;
+                        cout << actualId << endl;
+                        if (valeur==actualId){
+                            fileInd << ligneEvent << std::endl;
+
+
+                        }
+                    }
+                    cptt++;
+                }
+            }
+            fileInd.close();
+
+        }
+        fileEvent.close();
+        fileInds.close();
+
+
+
 
     }
+
+
     return 0;
 }
+
 
 
