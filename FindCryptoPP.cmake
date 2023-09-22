8bbe1b5c86b294eef322003c601705c02172bf92
valpaz <aguilar.jeandavid@yahoo.com>
2023-09-22 12:04:16 +0200

    suppression fichier inutile

--- FindCryptoPP.cmake
+++
@@ -1,7 +0,0 @@
-# Définissez un module FindCryptoPP personnalisé
-find_path(CRYPTOPP_INCLUDE_DIR cryptopp/cryptlib.h)
-find_library(CRYPTOPP_LIBRARY cryptopp)
-
-if (CRYPTOPP_INCLUDE_DIR AND CRYPTOPP_LIBRARY)
-    set(CRYPTOPP_FOUND TRUE)
-endif ()
